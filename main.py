ed58e9115f5a13aacd50de652f645fa661174e60
muchaslam7-design <muchaslam7@gmail.com>
2026-06-14 14:47:06 +0500

    Test CORS all

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://supplychain-frontend-app.vercel.app"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
